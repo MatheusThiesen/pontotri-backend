05620cec55c16a84498f866deea3ef94b26267a2
MatheusThiesen <matheusreis314@gmail.com>
2025-06-01 22:29:58 -0300

    feat: ordernar por dia da semana

--- src/infra/http/presenters/work-schedule-presenter.ts
+++ src/infra/http/presenters/work-schedule-presenter.ts
@@ -1,5 +1,15 @@
 import { WorkSchedule } from "@/domain/entities/work-schedule";
 
+const weekOrder = [
+  "MONDAY",
+  "TUESDAY",
+  "WEDNESDAY",
+  "THURSDAY",
+  "FRIDAY",
+  "SATURDAY",
+  "SUNDAY",
+];
+
 export class WorkSchedulePresenter {
   static toHTTP(workSchedule: WorkSchedule) {
     return {
@@ -7,17 +17,21 @@ export class WorkSchedulePresenter {
       name: workSchedule.name,
       companyId: workSchedule.companyId,
       updatedAt: workSchedule.updatedAt,
-      days: workSchedule.days.map((day) => ({
-        id: day.id.toValue(),
-        weekday: day.weekday,
-        startTime: day.startTime,
-        endTime: day.endTime,
-        totalWorkMinutes: day.totalWorkMinutes,
-        breakType: day.breakType,
-        breakStartWindow: day.breakStartWindow,
-        breakEndWindow: day.breakEndWindow,
-        breakDuration: day.breakDuration,
-      })),
+      days: workSchedule.days
+        .sort(
+          (a, b) => weekOrder.indexOf(a.weekday) - weekOrder.indexOf(b.weekday)
+        )
+        .map((day) => ({
+          id: day.id.toValue(),
+          weekday: day.weekday,
+          startTime: day.startTime,
+          endTime: day.endTime,
+          totalWorkMinutes: day.totalWorkMinutes,
+          breakType: day.breakType,
+          breakStartWindow: day.breakStartWindow,
+          breakEndWindow: day.breakEndWindow,
+          breakDuration: day.breakDuration,
+        })),
     };
   }
 }
